2b3d55a8926f21812cc93398d1e2eb7c255bd874
HaroldObasi <haroldobasi2k16@gmail.com>
2022-11-06 13:55:52 +0100

    added more errors to error handler

--- api/middlewares/error-handler.js
+++ api/middlewares/error-handler.js
@@ -1,11 +1,17 @@
 const ExistingUser = require("../errors/existing-user");
 const RequestValidationError = require("../errors/request-validation-error");
 const SignInError = require("../errors/sign-in-error");
+const UnauthorizedError = require("../errors/unauthorized-error");
 
 const errorHandler = (err, req, res, next) => {
     console.log("error middleware: ", err);
 
-    if (err instanceof RequestValidationError || ExistingUser || SignInError) {
+    if (
+        err instanceof RequestValidationError ||
+        ExistingUser ||
+        SignInError ||
+        UnauthorizedError
+    ) {
         res.status(err.statusCode).json({
             errors: err.serializeErrors(),
         });
