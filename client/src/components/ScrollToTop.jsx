08ab2a49bedde96784baabe9c34c609a1ed42785
HaroldObasi <haroldobasi2k16@gmail.com>
2023-01-12 16:22:57 +0100

    created scroll to top component

---
+++ client/src/components/ScrollToTop.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useLocation } from "react";
+
+const ScrollToTop = (props) => {
+  const location = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+
+  return <>{props.children}</>;
+};
+
+export default ScrollToTop;
